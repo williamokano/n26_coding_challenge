ab8a51b79db6ca310298f464e9699f11f817d61d
William Johnson dos Santos Okano <williamokano@gmail.com>
2019-05-25 15:43:14 -0300

    Added tests for TransactionsServiceImpl

--- src/test/groovy/com/n26/service/impl/TransactionsServiceImplTest.groovy
+++ src/test/groovy/com/n26/service/impl/TransactionsServiceImplTest.groovy
@@ -1,15 +1,34 @@
 package com.n26.service.impl
 
+import com.n26.config.ConfigResolver
+import com.n26.exception.TooOldTransactionException
 import com.n26.exception.TransactionIsInTheFutureException
 import com.n26.model.Transaction
-import com.n26.service.TransactionsService
+import com.n26.repository.TransactionsRepository
 import spock.lang.Specification
 
 import java.time.LocalDateTime
 
 class TransactionsServiceImplTest extends Specification {
+    ConfigResolver configResolver = Mock(ConfigResolver)
+    TransactionsRepository transactionsRepository = Mock(TransactionsRepository)
 
-    TransactionsService transactionService = new TransactionsServiceImpl()
+    TransactionsServiceImpl transactionService = new TransactionsServiceImpl(configResolver, transactionsRepository)
+
+    def "should save transactions"() {
+        given:
+        def transaction = Transaction.builder()
+                .amount(new BigDecimal("1000.00"))
+                .timestamp(LocalDateTime.now())
+                .build()
+
+        when:
+        transactionService.addTransaction(transaction)
+
+        then:
+        1 * configResolver.transactionsWindowInSeconds() >> 60
+        1 * transactionsRepository.save(transaction)
+    }
 
     def "should throw exception if transaction is in the future"() {
         given:
@@ -23,5 +42,20 @@ class TransactionsServiceImplTest extends Specification {
 
         then:
         thrown(TransactionIsInTheFutureException)
+    }
+
+    def "should throw exception if transaction is before the tumbling window"() {
+        given:
+        def transaction = Transaction.builder()
+                .amount(new BigDecimal("1000.00"))
+                .timestamp(LocalDateTime.now().minusHours(1L))
+                .build()
+
+        when:
+        transactionService.addTransaction(transaction)
+
+        then:
+        1 * configResolver.transactionsWindowInSeconds() >> 60
+        thrown(TooOldTransactionException)
     }
 }
